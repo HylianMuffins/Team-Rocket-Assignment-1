e3ef3e209ff5b893de8adce07994dd7db8ed49a4
CharumithaSelvaraj <113149369+CharumithaSelvaraj@users.noreply.github.com>
2022-09-27 09:01:44 -0600

    Create ControlFlow.py
    half of the control flow is finished, I am still editing the rest

---
+++ ControlFlow.py
@@ -0,0 +1,27 @@
+# this is code for control flow 
+# this was charu's code 
+
+
+if operator_one == '+' or "-" and operator_two == "*" or "/": 
+
+    if operator_two == "*":
+        stored_val = myNum(num1,num3)
+    else: 
+        stored_val = myDiv(num2,num3)
+    
+
+    if operator_one == "+":
+        final_val = myAdd(num1,stored_val)
+    else:
+        final_val = mySub(num1,stored_val)
+    
+    if operator_two == "+":
+        stored_val = myAdd(num1,num2)
+        if operator_two == "+":
+            
+    elif operator_one == "-":
+        
+
+
+
+    return final_val
